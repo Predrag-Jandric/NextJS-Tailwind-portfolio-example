667f758bf8af8f2cd8f015ca5359d0e6fcab6f51
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-14 23:24:57 +0200

    page transition DONE

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,26 @@
+import { motion } from "framer-motion";
+
+const transitionVariant = {
+  initial: {
+    x: "100%",
+    innerWidth: "100%"
+  },
+  animate: {
+    x: "0%",
+    width: "0%",
+  },
+  exit: {
+    x: ['0%', '100%'],
+    width: ['0%', '100%'],
+  }
+}
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]" variants={transitionVariant} initial="initial" animate="animate" exit="exit" transition={{ duration: 0.4, ease: 'easeInOut' }}>
+
+      </motion.div>
     </>
   );
 };
