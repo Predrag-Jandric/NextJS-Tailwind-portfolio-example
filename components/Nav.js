6918054bdb958c05dc51e3a691acc02c02bbf09c
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-16 20:28:52 +0200

    About page in progress

--- components/Nav.js
+++ components/Nav.js
@@ -39,7 +39,7 @@ const Nav = () => {
       <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-3 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
         {navData.map((link, index) => {
           return (
-            <Link className={`${link.path === pathname && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300`} key={index} href={link.path}>
+            <Link className={`${link.path === pathname && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300 text-2xl`} key={index} href={link.path}>
 
               {/* tooltip */}
               <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
--- pages/about/index.js
+++ pages/about/index.js
@@ -20,6 +20,7 @@ import Avatar from "../../components/Avatar";
 import Circles from "../../components/Circles";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
+import CountUp from 'react-countup'
 
 
 //  data
@@ -106,13 +107,57 @@ const About = () => {
         <Avatar />
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
-        <div>text</div>
+        {/* text */}
+        <div className="flex-1 flex flex-col justify-center">
+        <h2 className="h2">Captivating <span className="text-accent">stories</span> birth magnificent designs.</h2>
+        <p className="max-2-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate pariatur dolorem minima possimus dolores nisi veniam eius iure, voluptatibus omnis!</p>
+        {/* counters */}
         <div>
+          <div>
+            {/* experience */}
+            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0"></div>
+            <div className="text-5xl font-extrabold text-accent mb-4">
+              <CountUp start={0} end={99} duration={5} /> +
+            </div>
+            <div>Years of experience</div>
+          </div>
+        </div>
+        </div>
+        {/* info */}
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               return (
-                <div key={itemIndex} 
-                className={`${index === itemIndex && 'text-accent after:w-[100%] after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-2 after:-left-0`} onClick={() => setIndex(itemIndex)}>{item.title}</div>
+                <div
+                  key={itemIndex}
+                  className={`relative cursor-pointer capitalize xl:text-lg ${index === itemIndex
+                    ? 'text-accent after:w-full after:h-[2px] after:bg-white after:absolute after:-bottom-2 after:-left-0 after:transition-all after:duration-300'
+                    : 'after:w-0'
+                    }`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+
+          </div>
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div className="cla
+                flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/80" key={itemIndex}>
+                  {/* title */}
+                  <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                  <div className="hidden md:flex">-</div>
+                  <div>{item.stage}</div>
+                  <div className="flex gap-x-4">
+                    {/* icons */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return <div className="text-2xl text-white">{icon}</div>
+                    })}
+                  </div>
+                </div>
               )
             })}
           </div>
