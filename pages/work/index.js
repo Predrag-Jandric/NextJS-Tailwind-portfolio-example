3d901a8912a13531b6f77bcdfa15f0ed9b4c3e18
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-16 23:49:57 +0200

    WORK page complete

--- pages/work/index.js
+++ pages/work/index.js
@@ -9,6 +9,7 @@ import Bulb from "../../components/Bulb";
 import Circles from "../../components/Circles"
 import { motion } from "framer-motion";
 import {fadeIn} from "../../variants"
+import WorkSlider from "../../components/WorkSlider";
 
 
 const Work = () => {
@@ -19,9 +20,16 @@ const Work = () => {
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8">My Work</h2>
-            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED....</p>
+            <h2 className="h2 xl:mt-12">My Work</h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt eligendi esse corrupti. Cum, quisquam! Nemo, ex? Quia doloribus tempora aspernatur. Cum, quisquam! Nemo, ex? Quia doloribus tempora aspernatur</p>
           </div>
+
+          {/* slider */}
+          <div className="w-full xl:max-w-[65%]">
+          <WorkSlider/>
+          </div>
+
+
         </div>
       </motion.div>
       <Bulb/>
