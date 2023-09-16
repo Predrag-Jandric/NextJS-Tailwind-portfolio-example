bcc93345ee02e5eaee7d55c25155f85467c50fff
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-16 23:02:28 +0200

    Services page DONE. after some problems i decided to make it almost empty and customisable

--- pages/about/index.js
+++ pages/about/index.js
@@ -51,11 +51,11 @@ const aboutData = [
     info: [
       {
         title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
+        stage: '2000 - 2099',
       },
       {
         title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
+        stage: '2000 - 2099',
       },
     ],
   },
@@ -64,15 +64,15 @@ const aboutData = [
     info: [
       {
         title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        stage: '2000 - 2099',
       },
       {
         title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
+        stage: '2000 - 2099',
       },
       {
         title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
+        stage: '2000 - 2099',
       },
     ],
   },
@@ -81,15 +81,15 @@ const aboutData = [
     info: [
       {
         title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
+        stage: '2099',
       },
       {
         title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
+        stage: '2099',
       },
       {
         title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
+        stage: '2099',
       },
     ],
   },
@@ -102,9 +102,6 @@ const About = () => {
   return (
     <main className="h-full bg-primary/30 py-32 text-center xl:text-left">
       <Circles />
-      {/* <motion.div variants={fadeIn('right', 0.2)} initial="hidden" animate="show" exit="hidden" className="hidden xl:flex absolute bottom-0 -left-[370px]">
-        <Avatar />
-      </motion.div> */}
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-10">
         {/* Text */}
         <motion.div variants={fadeIn('right', 0.2)} initial="hidden" animate="show" exit="hidden" className=" flex-1 flex flex-col justify-center">
