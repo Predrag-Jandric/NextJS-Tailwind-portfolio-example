7a007576238fd797592fbca071d14d3dcf566552
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-16 23:30:06 +0200

    WORK page in progress

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,32 @@
+import {
+  RxCrop,
+  RxPencil2,
+  RxDesktop,
+  RxReader,
+  RxRocket,
+} from "react-icons/rx";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles"
+import { motion } from "framer-motion";
+import {fadeIn} from "../../variants"
+
+
 const Work = () => {
-  return <div>Work</div>;
-};
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <motion.div variants={fadeIn('right', 0.2)} initial="hidden" animate="show" exit="hidden" className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <h2 className="h2 xl:mt-8">My Work</h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED.... PAGE TO BE CUSTOMIZED....</p>
+          </div>
+        </div>
+      </motion.div>
+      <Bulb/>
+    </div>
+  )
+}
 
 export default Work;
