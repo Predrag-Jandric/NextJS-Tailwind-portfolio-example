fd16bd70335f52999556ad588ace6b6144e135a2
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-18 21:36:07 +0200

    CONTACT page complete

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,22 @@ import Layout from '../components/Layout';
 import Transition from '../components/Transition';
 import { useRouter } from 'next/router'
 import { AnimatePresence, motion } from 'framer-motion';
+import Head from 'next/head';
+
+
+export const metadata = {
+  title: "Portfolio Practice",
+  description: "NextJS & Tailwind. practice making Portfolio"
+}
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   return (
+    <>
+    <Head>
+      <title>{metadata.title}</title>
+      <meta name="description" content={metadata.description} />
+    </Head>
     <Layout>
       <AnimatePresence mode="wait">
         <motion.div key={router.route} className='h-full'>
@@ -15,6 +27,7 @@ function MyApp({ Component, pageProps }) {
         </motion.div>
       </AnimatePresence>
     </Layout>
+  </>
   );
 }
 
